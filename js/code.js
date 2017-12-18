1411297a76ba3f3314f3278560572d3c077a182e
maxl <maxl@mr-finch.lamboz.priv>
2017-12-18 12:31:12 +0100

    Maps are working...

---
+++ js/code.js
@@ -0,0 +1,56 @@
+var map;
+var ajaxRequest;
+var plotlist;
+var plotlayers=[];
+
+var mail = [
+	{"user" : "f.delucca3249a", "domain" : "ordpsicologier" },
+	{"user" : "francesca.delucca", "domain" : "psypec" }
+]
+
+function dom(n) {
+	var my_mail = "mailto:" +mail[n]["user"] + "@" + mail[n]["domain"]+".it";
+	window.location.href = my_mail
+
+}
+
+function initmap() {
+	// set up the map
+	map = new L.Map('map-bibbiano');
+
+	// create the tile layer with correct attribution
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 9, maxZoom: 19, attribution: osmAttrib});		
+
+	// start the map in South-East England
+	map.setView(new L.LatLng(44.66416, 10.47328),16);
+	map.addLayer(osm);
+
+	//add popup
+	var popup = L.popup()
+		.setLatLng([44.66416, 10.47328])
+		.setContent("<div class='popx'><b>Dott.ssa Francesca de Lucca</b><br>Tel. +393403946998</div>")
+		.openOn(map);
+}
+
+function initmap2() {
+	// set up the map
+	map = new L.Map('map-casalecchio');
+
+	// create the tile layer with correct attribution
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 9, maxZoom: 19, attribution: osmAttrib});		
+
+	// start the map in South-East England
+	map.setView(new L.LatLng(44.4777258,11.2749066),16);
+	map.addLayer(osm);
+
+	//add popup
+	var popup = L.popup()
+		.setLatLng([44.4777258,11.2749066])
+		.setContent("<div class='popx'><b>Dott.ssa Francesca de Lucca</b><br>Tel. +393403946998</div>")
+		.openOn(map);
+}
+
--- pages/contatti.md
+++ pages/contatti.md
@@ -4,12 +4,21 @@ title: Contatti
 permalink: /contatti/
 order: 99
 inner: 1
+usemap: 1
 ---
 
 ## Recapiti
 
 Indirizzo, telefono, mail
 
-## Cartine
+## Dove
 
-Cartina dello studio di Casalecchio e Bibbianox
+### Bibbiano (RE)
+
+<div class="mapbox" id="map-bibbiano"></div>
+
+
+### Casalecchio di Reno (BO)
+
+<div class="mapbox" id="map-casalecchio"></div>
+
